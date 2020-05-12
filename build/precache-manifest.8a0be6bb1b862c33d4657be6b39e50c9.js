50e52c84ea2e4878923b06a6cfcca0a6d79a8eb6
Daniel Gibbons <gibbonscomedy@gmail.com>
2020-05-12 17:02:44 -0400

    fixed start script

---
+++ build/precache-manifest.8a0be6bb1b862c33d4657be6b39e50c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8409c18c27084487ca945a49e7710f3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "039a803f618a64bf860a",
+    "url": "/static/css/main.1f88ed2b.chunk.css"
+  },
+  {
+    "revision": "fb536641eb9084ce5c37",
+    "url": "/static/js/2.9d958aa2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9d958aa2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "039a803f618a64bf860a",
+    "url": "/static/js/main.281ca332.chunk.js"
+  },
+  {
+    "revision": "8634a711f4dc8df9cf9b",
+    "url": "/static/js/runtime-main.7c217db9.js"
+  }
+]);
